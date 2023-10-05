5bddb94923b8092274bdde6cfde3fcd0df690c7b
ogtitle724 <wje0724@gmail.com>
2023-10-06 03:28:36 +0900

    delete useMemo

--- src/app/(community)/_components/header/components/sign/signup/SignUp.js
+++ src/app/(community)/_components/header/components/sign/signup/SignUp.js
@@ -1,7 +1,7 @@
 "use client";
 import Fetch from "@/util/fetch";
 import socket from "@/util/socket";
-import { useCallback, useEffect, useState, useMemo } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { checkUid, checkNick, checkEmail, checkPwd } from "@/util/validation";
 import "./style.css";
 
@@ -111,155 +111,6 @@ export default function SignUp({ setShowSignUpForm }) {
     }
   };
 
-  const memoSectionInputUid = useMemo(() => {
-    return (
-      <SectionInput
-        placeholder={"아이디 (3~20자 영문/숫자)"}
-        target={uid}
-        handleOnChange={(e) => {
-          setUid(e.target.value);
-          handleCheckDuplication(e.target.value, "uid");
-        }}
-      >
-        <p
-          className={
-            "signup__warn" +
-            (checkUid(uid) && canUseUid ? " signup__warn--ok" : "")
-          }
-        >
-          {uid &&
-            (checkUid(uid)
-              ? canUseUid
-                ? "사용 가능한 아이디입니다."
-                : "이미 사용중인 아이디입니다."
-              : "아이디가 양식에 맞지 않습니다.")}
-        </p>
-      </SectionInput>
-    );
-  }, [canUseUid, handleCheckDuplication, uid]);
-
-  const memoSectionInputNick = useMemo(() => {
-    return (
-      <SectionInput
-        placeholder={"닉네임 (3~8자 한글/영문/숫자)"}
-        target={nick}
-        handleOnChange={(e) => {
-          setNick(e.target.value);
-          handleCheckDuplication(e.target.value, "nick");
-        }}
-      >
-        <p
-          className={
-            "signup__warn" +
-            (checkNick(nick) && canUseNick ? " signup__warn--ok" : "")
-          }
-        >
-          {nick &&
-            (checkNick(nick)
-              ? canUseNick
-                ? "사용 가능한 닉네임입니다."
-                : "이미 사용중인 닉네임입니다."
-              : "닉네임이 양식에 맞지 않습니다.")}
-        </p>
-      </SectionInput>
-    );
-  }, [canUseNick, handleCheckDuplication, nick]);
-
-  const memoSectionInputEmail = useMemo(() => {
-    return (
-      <SectionInput
-        placeholder={"이메일 (example@email.com)"}
-        target={email}
-        handleOnChange={(e) => setEmail(e.target.value)}
-      >
-        <button
-          className={"signup__btn-auth"}
-          onClick={handleClickBtnAuth}
-          disabled={isAuthBtnDisabled || !checkEmail(email)}
-        >
-          {count ? `${~~(count / 60)} : ${count % 60}` : "인증"}
-        </button>
-        <section className="signup__section-auth">
-          <input
-            type="text"
-            className={
-              "signup__input-auth" +
-              (isClickBtnAuth ? "" : " signup__input-auth--disable") +
-              (isClickBtnAuth && isFail ? " signup__input-auth--fail" : "")
-            }
-            value={authCode}
-            onChange={(e) => setAuthCode(e.target.value)}
-            autoComplete="off"
-          ></input>
-          <button
-            className={
-              "signup__btn-auth" +
-              (isClickBtnAuth
-                ? " signup__btn-auth--active"
-                : " signup__btn-auth--disable")
-            }
-            onClick={handleClickBtnConfrim}
-            type="button"
-            disabled={isCodeValid}
-          >
-            {isCodeValid ? "✔" : "확인"}
-          </button>
-        </section>
-      </SectionInput>
-    );
-  }, [
-    authCode,
-    count,
-    email,
-    handleClickBtnAuth,
-    handleClickBtnConfrim,
-    isAuthBtnDisabled,
-    isClickBtnAuth,
-    isCodeValid,
-    isFail,
-  ]);
-
-  const memoSectionInputPwd = useMemo(() => {
-    return (
-      <SectionInput
-        placeholder={"비밀번호 (8~16자 영문+숫자+특수기호)"}
-        target={pwd}
-        handleOnChange={(e) => setPwd(e.target.value)}
-      >
-        <p
-          className={
-            "signup__warn" + (checkPwd(pwd) ? " signup__warn--ok" : "")
-          }
-        >
-          {pwd &&
-            (checkPwd(pwd)
-              ? "사용 가능한 비밀번호입니다."
-              : "비밀번호가 양식에 맞지 않습니다.")}
-        </p>
-      </SectionInput>
-    );
-  }, [pwd]);
-
-  const memoSectionInputPwdConfirm = useMemo(() => {
-    return (
-      <SectionInput
-        placeholder={"비밀번호 (중복확인)"}
-        target={confirmPwd}
-        handleOnChange={(e) => {
-          setConfirmPwd(e.target.value);
-          handlePwdsMatch(e.target.value, pwd);
-        }}
-      >
-        <p className={"signup__warn" + (isPwdMatch ? " signup__warn--ok" : "")}>
-          {confirmPwd &&
-            (isPwdMatch
-              ? "비밀번호가 일치합니다"
-              : "입력한 비밀번호가 일치하지 않습니다.")}
-        </p>
-      </SectionInput>
-    );
-  }, [confirmPwd, handlePwdsMatch, isPwdMatch, pwd]);
-
   return (
     <div
       className="signup__background"
@@ -273,11 +124,122 @@ export default function SignUp({ setShowSignUpForm }) {
           className="btn-close icon"
           onClick={() => setShowSignUpForm(false)}
         ></button>
-        {memoSectionInputUid}
-        {memoSectionInputNick}
-        {memoSectionInputEmail}
-        {memoSectionInputPwd}
-        {memoSectionInputPwdConfirm}
+        <SectionInput
+          placeholder={"아이디 (3~20자 영문/숫자)"}
+          target={uid}
+          handleOnChange={(e) => {
+            setUid(e.target.value);
+            handleCheckDuplication(e.target.value, "uid");
+          }}
+        >
+          <p
+            className={
+              "signup__warn" +
+              (checkUid(uid) && canUseUid ? " signup__warn--ok" : "")
+            }
+          >
+            {uid &&
+              (checkUid(uid)
+                ? canUseUid
+                  ? "사용 가능한 아이디입니다."
+                  : "이미 사용중인 아이디입니다."
+                : "아이디가 양식에 맞지 않습니다.")}
+          </p>
+        </SectionInput>
+        <SectionInput
+          placeholder={"닉네임 (3~8자 한글/영문/숫자)"}
+          target={nick}
+          handleOnChange={(e) => {
+            setNick(e.target.value);
+            handleCheckDuplication(e.target.value, "nick");
+          }}
+        >
+          <p
+            className={
+              "signup__warn" +
+              (checkNick(nick) && canUseNick ? " signup__warn--ok" : "")
+            }
+          >
+            {nick &&
+              (checkNick(nick)
+                ? canUseNick
+                  ? "사용 가능한 닉네임입니다."
+                  : "이미 사용중인 닉네임입니다."
+                : "닉네임이 양식에 맞지 않습니다.")}
+          </p>
+        </SectionInput>
+        <SectionInput
+          placeholder={"이메일 (example@email.com)"}
+          target={email}
+          handleOnChange={(e) => setEmail(e.target.value)}
+        >
+          <button
+            className={"signup__btn-auth"}
+            onClick={handleClickBtnAuth}
+            disabled={isAuthBtnDisabled || !checkEmail(email)}
+          >
+            {count ? `${~~(count / 60)} : ${count % 60}` : "인증"}
+          </button>
+          <section className="signup__section-auth">
+            <input
+              type="text"
+              className={
+                "signup__input-auth" +
+                (isClickBtnAuth ? "" : " signup__input-auth--disable") +
+                (isClickBtnAuth && isFail ? " signup__input-auth--fail" : "")
+              }
+              value={authCode}
+              onChange={(e) => setAuthCode(e.target.value)}
+              autoComplete="off"
+            ></input>
+            <button
+              className={
+                "signup__btn-auth" +
+                (isClickBtnAuth
+                  ? " signup__btn-auth--active"
+                  : " signup__btn-auth--disable")
+              }
+              onClick={handleClickBtnConfrim}
+              type="button"
+              disabled={isCodeValid}
+            >
+              {isCodeValid ? "✔" : "확인"}
+            </button>
+          </section>
+        </SectionInput>
+        <SectionInput
+          placeholder={"비밀번호 (8~16자 영문+숫자+특수기호)"}
+          target={pwd}
+          handleOnChange={(e) => setPwd(e.target.value)}
+        >
+          <p
+            className={
+              "signup__warn" + (checkPwd(pwd) ? " signup__warn--ok" : "")
+            }
+          >
+            {pwd &&
+              (checkPwd(pwd)
+                ? "사용 가능한 비밀번호입니다."
+                : "비밀번호가 양식에 맞지 않습니다.")}
+          </p>
+        </SectionInput>
+        <SectionInput
+          placeholder={"비밀번호 (중복확인)"}
+          target={confirmPwd}
+          handleOnChange={(e) => {
+            setConfirmPwd(e.target.value);
+            handlePwdsMatch(e.target.value, pwd);
+          }}
+        >
+          <p
+            className={"signup__warn" + (isPwdMatch ? " signup__warn--ok" : "")}
+          >
+            {confirmPwd &&
+              (isPwdMatch
+                ? "비밀번호가 일치합니다"
+                : "입력한 비밀번호가 일치하지 않습니다.")}
+          </p>
+        </SectionInput>
         <button
           type="submit"
           className="signup__btn"
