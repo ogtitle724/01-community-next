eaeb87d123a30ab11fa3f1f8e9cba2d2a9fa1b48
ogtitle724 <wje0724@gmail.com>
2023-11-03 15:25:53 +0900

    .

--- src/app/(community)/write/item/page.js
+++ src/app/(community)/write/item/page.js
@@ -49,7 +49,7 @@ export default function ItemUpload() {
 
       await Fetch.post(process.env.NEXT_PUBLIC_PATH_ITEM, body, option);
       router.refresh();
-      router.push(process.env.NEXT_PUBLIC_ROUTE_SHOWCASE);
+      router.push(process.env.NEXT_PUBLIC_ROUTE_BARTER);
     } catch (err) {
       console.error(err);
     }
